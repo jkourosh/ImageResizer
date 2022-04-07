49c362898850e371d7e5eb656d706d001c74930d
jkourosh <jkourosh@gmail.com>
2022-04-07 19:34:09 +0430

    refac

--- imageresize.py
+++ imageresize.py
@@ -4,18 +4,19 @@ import PIL;
 import sys,os,shutil;
 import glob;
 
-def resize_image(input_dir, percentage=50 , max_size=2000000 ,width=None, height=None):
+def resize_image(input_dir, percentage=70 , min_size=2000000 ,width=None, height=None):
+  
     input_dir = input_dir+'/'
     output_dir = input_dir + "resized/"
     images = [file for file in os.listdir(input_dir) if file.endswith(('jpeg', 'png', 'jpg')) ]
     if not os.path.exists(output_dir):
         os.makedirs(output_dir,exist_ok=True)
     for infile in images: 
-        print(infile)
         file, ext = os.path.splitext(infile)
         fullfile = input_dir + infile
         img = Image.open(input_dir + infile)
-        if os.path.getsize(fullfile)>max_size:   
+        if os.path.getsize(fullfile)>min_size:   
+           print(infile)
            width = int(img.size[0] *percentage/100)
            height = int(img.size[1]*percentage/100)
            size = width, height
@@ -28,5 +29,6 @@ def resize_image(input_dir, percentage=50 , max_size=2000000 ,width=None, height
 
 
 if __name__ == "__main__":
-    resize_image(input_dir, percentage)
-
+   
+    resize_image(input_dir, percentage, min_size,width, height)
+  
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from tkinter import filedialog
+import subprocess
+
 from tkinter import *
 from imageresize import resize_image
-
-def resizer():
+import sys
+def browse():
     # Allow user to select a directory and store it in global var
     # called folder_path
     global folder_path
     filename = filedialog.askdirectory()
     folder_path.set(filename)
-    ret = resize_image(folder_path.get(), int(txtPer.get()), int(txtMin.get()))
-    if ret:
-      print('Done!')
-      lblComp = Label(window,text='Done !')
+    
      
-   
+def submit():
+  ret = resize_image(folder_path.get(), int(perV.get()), int(minV.get()))
+  openfolder=folder_path.get()+'/resized'
+  if ret:
+    print('Done!')
+    lblComp = Label(window,text='Done !')
+    # subprocess.Popen(r'explorer /select,'+'"' openfolder+'"', shell=True)
 
 
 window = Tk()
@@ -21,13 +26,17 @@ window.title("Resize Images")
 window.geometry('400x300')
 window.resizable(0, 0)
 folder_path = StringVar()
+perV=StringVar()
+minV=StringVar()
+perV.set(70)
+minV.set(2000000)
 lblPer = Label(window,text= 'Percentage :')
 lblPer.place(x=10,y=10)
-txtPer = Entry(window,width=2)
+txtPer = Entry(window,width=4 ,textvariable=perV )
 txtPer.place(x=90, y=10)
 lblMin = Label(window,text= 'Min Size :')
 lblMin.place(x=220,y=10)
-txtMin = Entry(window, width=10)
+txtMin = Entry(window, width=10 ,textvariable= minV)
 txtMin.place(x=290, y=10)
 lblPath = Label(window,text='Path :')
 lblPath.place(x=10,y=40)
@@ -35,8 +44,12 @@ lblComp = Label(window,text='Select a directory')
 lblComp.place(x=150,y=120)
 txtPath = Entry(window,textvariable=folder_path, width=50)
 txtPath.place(x=70,y=40)
-button2 = Button(text="Browse", command=resizer)
-button2.place(x=170,y=80)
+btnPath = Button(text="Browse", command=browse)
+btnPath.place(x=170,y=80)
+btnRun = Button(text="Resize", command=submit)
+btnRun.place(x=250,y=80)
+btnExit = Button(text="Exit", command=sys.exit)
+btnExit.place(x=170,y=200)
 
 
 window.mainloop()
