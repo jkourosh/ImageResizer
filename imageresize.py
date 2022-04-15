34ccf347c536993e19deef47713f04d23079c286
jkourosh <jkourosh@gmail.com>
2022-04-15 15:30:32 +0430

    ref 1

--- imageresize.py
+++ imageresize.py
@@ -1,34 +1,43 @@
-
+import glob
 from PIL import Image;
-import PIL;
-import sys,os,shutil;
-import glob;
+import os,shutil
+ext_list =('jpeg','JPEG', 'png','PNG', 'jpg','JPG','tif','TIF')
 
-def resize_image(input_dir, percentage=70 , min_size=2000000 ,width=None, height=None):
-  
-    input_dir = input_dir+'/'
-    output_dir = input_dir + "resized/"
-    images = [file for file in os.listdir(input_dir) if file.endswith(('jpeg', 'png', 'jpg')) ]
-    if not os.path.exists(output_dir):
+def resize_image(input_dir, percentage=70 , min_size=2 ,del_original=False):
+  min_size= int(min_size * 1000000)
+  dirpaths = [dirpath for dirpath,dirs,files in os.walk(input_dir) if not dirpath.endswith(('resized'))]
+  for dirpath in dirpaths:
+    dirpath=dirpath+"/"
+    output_dir = dirpath
+    if not del_original:
+      output_dir += "resized/"
+    images = [file for file in os.listdir(dirpath) if file.endswith(ext_list)]
+    for image in images:
+      if not os.path.exists(output_dir):
         os.makedirs(output_dir,exist_ok=True)
-    for infile in images: 
-        file, ext = os.path.splitext(infile)
-        fullfile = input_dir + infile
-        img = Image.open(input_dir + infile)
-        if os.path.getsize(fullfile)>min_size:   
-           print(infile)
-           width = int(img.size[0] *percentage/100)
-           height = int(img.size[1]*percentage/100)
-           size = width, height
-           img_resize = img.resize(size,Image.Resampling.NEAREST)
-           img_resize.save( output_dir + file  + ext)
-        else:
-           shutil.copy(fullfile, output_dir)
-    return True  
+      file, ext = os.path.splitext(image)
+      fullfile = dirpath + image
+      img = Image.open(fullfile)
+      if os.path.getsize(fullfile) > min_size: 
+        imgSize = img.size[0], img.size[1]   # width, height
+        newSize = sizeCalc(percentage, imgSize)
+        img_resize = img.resize(newSize,Image.Resampling.NEAREST)
+        img_resize.save( output_dir + file  + ext)
+        print('Resized & Copy: ',image)
+      else:
+        shutil.copy(fullfile, output_dir)
+        print('Just Copied: ',image)
+    movies = [file for file in os.listdir(dirpath) if file.endswith(('mp4','MP4', 'avi','AVI', '3gp','3GP')) ]  
+    if not del_original:     
+      for infile in movies:
+        shutil.copy(dirpath + infile, output_dir)
+        print('Just Copied: ',infile)  
+  print('-- Done! --')      
+  return True
 
-
-
-if __name__ == "__main__":
-   
-    resize_image(input_dir, percentage, min_size,width, height)
-  
+def sizeCalc(percentage, imgSize):
+    width  = int(imgSize[0] * percentage/100)
+    height = int(imgSize[1] * percentage/100)
+    iSize = width, height
+    return iSize
+ 
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 from tkinter import filedialog
-import subprocess
-
 from tkinter import *
+from setuptools import Command
 from imageresize import resize_image
 import sys
-def browse():
-    # Allow user to select a directory and store it in global var
-    # called folder_path
-    global folder_path
-    filename = filedialog.askdirectory()
-    folder_path.set(filename)
+
+# def changeMBText(event):
+  # print(minV.get())
+  # print(str((int(minV.get())/1000000))+' MB')
+  # lblMbS.config(text=str((int(minV.get())/1000000))+' MB')
+  # lblMbS.update()
+
+def submit():
+  changeStateText('Resizing...')
+  ret = resize_image(folder_path.get(), int(perV.get()), float(minV.get()),delV.get())
+  if ret:
+    changeStateText('Done !')
     
      
-def submit():
-  ret = resize_image(folder_path.get(), int(perV.get()), int(minV.get()))  
-  openfolder=folder_path.get()+'/resized'
-  if ret:
-    print('Done!')
-    lblComp = Label(window,text='Done !')
-    # subprocess.Popen(r'explorer /select,'+'"' openfolder+'"', shell=True)
+def changeStateText(txt):
+  lblComp.config(text=txt)
+  lblComp.update()
+    
 
-
+def browse():
+  # Allow user to select a directory and store it in global var
+  # called folder_path
+  global folder_path
+  filename = filedialog.askdirectory()
+  folder_path.set(filename)
+    
+ 
 window = Tk()
 window.title("Resize Images")
 window.geometry('400x300')
@@ -28,31 +37,42 @@ window.resizable(0, 0)
 folder_path = StringVar()
 perV=StringVar()
 minV=StringVar()
+mbV=StringVar()
+delV=BooleanVar()
 perV.set(70)
-minV.set(2000000)
+minV.set(2)
 lblPer = Label(window,text= 'Percentage :')
 lblPer.place(x=10,y=10)
 txtPer = Entry(window,width=4 ,textvariable=perV )
 txtPer.place(x=90, y=10)
 lblMin = Label(window,text= 'Min Size :')
-lblMin.place(x=220,y=10)
-txtMin = Entry(window, width=10 ,textvariable= minV)
-txtMin.place(x=290, y=10)
-lblPath = Label(window,text='Path :')
-lblPath.place(x=10,y=40)
+lblMin.place(x=135,y=10)
+txtMin = Entry(window, width=4 ,textvariable= minV)
+txtMin.place(x=195, y=10)
+lblMb = Label(window,text= 'MB')
+lblMb.place(x=220,y=10)
+lblFolder = Label(window,text='Folder :' )
+lblFolder.place(x=10,y=40)
 lblComp = Label(window,text='Select a directory')
 lblComp.place(x=150,y=120)
-txtPath = Entry(window,textvariable=folder_path, width=50)
-txtPath.place(x=70,y=40)
-btnPath = Button(text="Browse", command=browse)
-btnPath.place(x=170,y=80)
-btnRun = Button(text="Resize", command=submit)
-btnRun.place(x=250,y=80)
-btnExit = Button(text="Exit", command=sys.exit)
+txtFolder = Entry(window,textvariable=folder_path, width=40)
+txtFolder.place(x=60,y=40)
+chkDelete = Checkbutton(window, fg='red', text="Delete Original", variable= delV)
+chkDelete.place(x=10,y=70)
+btnFolder = Button(window,text="Browse", command=browse)
+btnFolder.place(x=315,y=37)
+btnRun = Button(window,text="Resize", command=submit)
+btnRun.place(x=180,y=80)
+btnExit = Button(window,text="Exit", command=sys.exit)
 btnExit.place(x=170,y=200)
-
-
 window.mainloop()
+
+
+
+
+
+
+
 
 
 
